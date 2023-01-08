d3b037dfc3b5f2775a213b0277666c7ca3350ded
CAPTAIN NEMO <nimanamjouyan@gmail.com>
2023-01-08 15:29:13 +0000

    ♻️ Refactored code into appropriate files to keep the code DRY.

--- faker/providers/cron/__init__.py
+++ faker/providers/cron/__init__.py
@@ -1,130 +1,18 @@
-from typing import Dict, Tuple, List, Literal, Set
-from random import randint, choice
-
 from .. import BaseProvider
+from .models import Minute, Hour, DayOfMonth, Month, DayOfWeek
 
 
 class Provider(BaseProvider):
-    ALL: str = "*"
-    ANY: str = "?"
+    minute: Minute = Minute()
+    hour: Hour = Hour()
+    day_of_month: DayOfMonth = DayOfMonth()
+    month: Month = Month()
+    day_of_week: DayOfWeek = DayOfWeek()
 
-    RANGES: Dict[str, List[int]] = {
-        "minutes": [0, 59],
-        "hours": [0, 23],
-        "day_of_month": [1, 31],
-        "month": [1, 12],
-        "day_of_week": [0, 6],
-    }
-
-    MONTHS_ALPHA_NUMERIC_MAPPING: Dict[int, str] = {
-        1: "jan",
-        2: "feb",
-        3: "mar",
-        4: "apr",
-        5: "may",
-        6: "jun",
-        7: "jul",
-        8: "aug",
-        9: "sep",
-        10: "oct",
-        11: "nov",
-        12: "dec",
-    }
-
-    WEEK_DAYS_ALPHA_NUMERIC_MAPPING: Dict[int, str] = {
-        0: "sun",
-        1: "mon",
-        2: "tue",
-        3: "wed",
-        4: "thu",
-        5: "fri",
-        6: "sat",
-    }
-
-    def _create_random_integer_range_as_tuple(self, integer_range: List[int]) -> Tuple[int, int]:
-        starting_integer: int = randint(*integer_range)
-        ending_integer: int = randint(starting_integer, integer_range[-1])
-
-        return starting_integer, ending_integer
-
-    def _create_list_of_unique_integers_within_a_range(self, integer_range: List[int], length: int) -> List[int]:
-        set_of_integers: Set[int] = set()
-
-        while len(set_of_integers) < length:
-            set_of_integers.add(randint(*integer_range))
-
-        return list(set_of_integers)
-
-    def _create_nth_last_day_of_month_string(self) -> str:
-        return f"""L-{randint(*Provider.RANGES["day_of_month"])}"""
-
-    def _create_nth_last_day_of_week_string(self) -> str:
-        return f"""{randint(*Provider.RANGES["day_of_week"])}L"""
-
-    def _create_day_of_month_expression(self):
-        return f"""{randint(*Provider.RANGES["day_of_month"])}W"""
-
-    def _create_nth_weekday_expression(self):
-        return f"""{randint(*Provider.RANGES["day_of_week"])}#{randint(1,5)}"""
-
-    def _convert_tuple_range_to_increment_or_range_string(
-        self, tuple_range: Tuple[int, int], delimiter: Literal["-", "/"]
-    ) -> str:
-        return f"{tuple_range[0]}{delimiter}{tuple_range[1]}"
-
-    def _convert_list_to_delimited_string(self, integer_list: List[int]) -> str:
-        return ",".join(str(item) for item in integer_list)
-
-    def _convert_week_or_month_integer_list_to_delimited_string_mapping(
-        self,
-        integer_list: List[int],
-        index_interpretation: Literal["weekday", "month"],
-        make_result_uniform: bool = True,
-    ) -> str:
-        string_mapping_list: List[str] = []
-
-        for item in integer_list:
-            if make_result_uniform:
-                string_mapping: str = self._convert_week_or_month_integer_to_string_mapping(item, index_interpretation)
-            else:
-                string_mapping: str = str(item)
-            string_mapping_list.append(string_mapping)
-
-        return self._convert_integer_list_to_delimited_string(string_mapping_list)
-
-    def _convert_week_or_month_integer_to_string_mapping(
-        self, key: int, index_interpretation: Literal["weekday", "month"]
-    ) -> str:
-        if index_interpretation == "month":
-            return Provider.MONTHS_ALPHA_NUMERIC_MAPPING[key]
-        else:
-            return Provider.WEEK_DAYS_ALPHA_NUMERIC_MAPPING[key]
-
-    def _convert_tuple_range_to_week_month_string_mapping(
-        self,
-        integer_tuple: Tuple[int, int],
-        index_interpretation: Literal["weekday", "month"],
-        delimiter: Literal["-", "/"],
-        is_start_string_mapping: bool = True,
-        is_end_string_mapping: bool = True,
-    ) -> str:
-        if is_start_string_mapping:
-            start: str = self._convert_week_or_month_integer_to_string_mapping(integer_tuple[0], index_interpretation)
-        else:
-            start: str = str(integer_tuple[0])
-        if is_end_string_mapping:
-            end: str = self._convert_week_or_month_integer_to_string_mapping(integer_tuple[1], index_interpretation)
-        else:
-            end: str = str(integer_tuple[1])
-
-        return f"{start}{delimiter}{end}"
-
-    def _create_random_minutes(self):
-        choice(
-            [
-                str(randint(*Provider.RANGES["minutes"])),
-                self._convert_tuple_range_to_string(
-                    self._create_random_integer_range_as_tuple(Provider.RANGES["minutes"])
-                ),
-            ]
+    def create_full_cron_expression(self) -> str:
+        return (
+            f"{Provider.minute.create_random_minute()} {Provider.hour.create_random_hour()} "
+            f"{Provider.day_of_month.create_random_day_of_month()} "
+            f"{Provider.month.create_random_month()} "
+            f"{Provider.day_of_week.create_random_day_of_week()}"
         )
--- faker/providers/cron/models.py
+++ faker/providers/cron/models.py
@@ -1,213 +1,148 @@
-from typing import List, Dict, Literal, Tuple
+from typing import List, Dict, Union, Callable
 from random import randint, choice
 
 from .base import Base
 
 
 class Minute(Base):
-    minute_range: List[int] = [0, 59]
+    def __init__(self):
+        super(Minute, self).__init__()
+        self.minute_range: List[int] = [0, 59]
+        self.random_choices: Union[Callable, str] = [
+            self.ALL,
+            self.ANY,
+            str(randint(*self.minute_range)),
+            lambda: self.convert_tuple_range_to_increment_or_range_string(
+                self.create_random_integer_range_as_tuple(self.minute_range), choice(["-", "/"])
+            ),
+            lambda: self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*self.minute_range)), "/"),
+            lambda: self.convert_list_to_delimited_string(
+                self.create_list_of_unique_integers_within_a_range(self.minute_range, randint(2, randint(3, 10)))
+            ),
+        ]
 
-    def create_random_minute(self, list_length: int) -> str:
-        return choice(
-            [
-                self.ALL,
-                self.ANY,
-                str(randint(*Minute.minute_range)),
-                self.convert_tuple_range_to_increment_or_range_string(
-                    self.create_random_integer_range_as_tuple(Minute.minute_range), choice(["-", "/"])
-                ),
-                self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*Minute.minute_range)), "/"),
-                # TODO: For everything below add a case where a comma separated list can be generated that also
-                # includes cases such as */5,4,7-9 * * * *.
-                self.convert_list_to_delimited_string(
-                    self.create_list_of_unique_integers_within_a_range(Minute.minute_range, randint(2, list_length))
-                ),
-            ]
-        )
+    def create_random_minute(self) -> str:
+        return self.select_random_option_from_list(self.random_choices)
 
 
 class Hour(Base):
-    hour_range: List[int] = [0, 23]
+    def __init__(self):
+        super(Hour, self).__init__()
+        self.hour_range: List[int] = [0, 23]
+        self.random_choices: Union[Callable, str] = [
+            self.ALL,
+            self.ANY,
+            str(randint(self.hour_range)),
+            lambda: self.convert_tuple_range_to_increment_or_range_string(
+                self.create_random_integer_range_as_tuple(self.hour_range), choice(["-", "/"])
+            ),
+            lambda: self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*self.hour_range)), "/"),
+            lambda: self.convert_list_to_delimited_string(
+                self.create_list_of_unique_integers_within_a_range(self.hour_range, randint(2, randint(3, 10)))
+            ),
+        ]
 
-    def create_random_hour(self, list_length: int) -> str:
-        return choice(
-            [
-                self.ALL,
-                self.ANY,
-                str(randint(*Hour.hour_range)),
-                self.convert_tuple_range_to_increment_or_range_string(
-                    self.create_random_integer_range_as_tuple(Hour.hour_range), choice(["-", "/"])
-                ),
-                self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*Hour.hour_range)), "/"),
-                self.convert_list_to_delimited_string(
-                    self.create_list_of_unique_integers_within_a_range(Hour.hour_range, randint(2, list_length))
-                ),
-            ]
-        )
+    def create_random_hour(self) -> str:
+        return self.select_random_option_from_list(self.random_choices)
 
 
 class DayOfMonth(Base):
-    day_of_month_range: List[int] = [1, 31]
+    def __init__(self):
+        super(DayOfMonth, self).__init__()
+        self.day_of_month_range: List[int] = [1, 31]
+        self.random_choices: Union[Callable, str] = [
+            self.ALL,
+            self.ANY,
+            str(randint(self.day_of_month_range)),
+            lambda: self.convert_tuple_range_to_increment_or_range_string(
+                self.create_random_integer_range_as_tuple(self.day_of_month_range), choice(["-", "/"])
+            ),
+            lambda: self.convert_tuple_range_to_increment_or_range_string(
+                (self.ALL, randint(*self.day_of_month_range)), "/"
+            ),
+            lambda: self.convert_list_to_delimited_string(
+                self.create_list_of_unique_integers_within_a_range(self.day_of_month_range, randint(2, randint(3, 10)))
+            ),
+            lambda: self.create_day_of_month_expression(),
+            lambda: self.create_nth_last_day_of_month_string(),
+        ]
 
-    def create_day_of_month_expression(self):
-        return f"""{randint(*DayOfMonth.day_of_month_range)}W"""
-
-    def create_nth_last_day_of_month_string(self) -> str:
-        return f"""L-{randint(*DayOfMonth.day_of_month_range)}"""
-
-    def create_random_day_of_month(self, list_length: int) -> str:
-        return choice(
-            [
-                self.ALL,
-                self.ANY,
-                str(randint(*DayOfMonth.day_of_month_range)),
-                self.convert_tuple_range_to_increment_or_range_string(
-                    self.create_random_integer_range_as_tuple(DayOfMonth.day_of_month_range), choice(["-", "/"])
-                ),
-                self.convert_tuple_range_to_increment_or_range_string(
-                    (self.ALL, randint(*DayOfMonth.day_of_month_range)), "/"
-                ),
-                self.convert_list_to_delimited_string(
-                    self.create_list_of_unique_integers_within_a_range(
-                        DayOfMonth.day_of_month_range, randint(2, list_length)
-                    )
-                ),
-                self.create_day_of_month_expression(),
-                self.create_nth_last_day_of_month_string(),
-            ]
-        )
+    def create_random_day_of_month(self) -> str:
+        return self.select_random_option_from_list(self.random_choices)
 
 
 class Month(Base):
-    month_range: List[int] = [1, 12]
+    def __init__(self):
+        super(Month, self).__init__()
+        self.month_range: List[int] = [1, 12]
 
-    months_alpha_numeric_mapping: Dict[int, str] = {
-        1: "jan",
-        2: "feb",
-        3: "mar",
-        4: "apr",
-        5: "may",
-        6: "jun",
-        7: "jul",
-        8: "aug",
-        9: "sep",
-        10: "oct",
-        11: "nov",
-        12: "dec",
-    }
+        self.months_alpha_numeric_mapping: Dict[int, str] = {
+            1: "jan",
+            2: "feb",
+            3: "mar",
+            4: "apr",
+            5: "may",
+            6: "jun",
+            7: "jul",
+            8: "aug",
+            9: "sep",
+            10: "oct",
+            11: "nov",
+            12: "dec",
+        }
+        self.random_choices: Union[Callable, str] = [
+            self.ALL,
+            self.ANY,
+            str(randint(self.month_range)),
+            lambda: self.convert_tuple_range_to_increment_or_range_string(
+                self.create_random_integer_range_as_tuple(self.month_range), choice(["-", "/"])
+            ),
+            lambda: self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*self.month_range)), "/"),
+            lambda: self.convert_list_to_delimited_string(
+                self.create_list_of_unique_integers_within_a_range(self.month_range, randint(2, randint(3, 10)))
+            ),
+            lambda: self.convert_integer_list_to_delimited_string_mapping(
+                self.create_list_of_unique_integers_within_a_range(self.month_range, randint(2, randint(3, 10))),
+                self.months_alpha_numeric_mapping
+                choice([True, False]),
+            ),
+        ]
 
-    def convert_month_integer_to_string_mapping(self, key: int) -> str:
-        return Month.months_alpha_numeric_mapping[key]
-
-    def convert_month_integer_list_to_delimited_string_mapping(
-        self,
-        integer_list: List[int],
-        make_result_uniform: bool = True,
-    ) -> str:
-        string_mapping_list: List[str] = []
-
-        for item in integer_list:
-            if make_result_uniform:
-                string_mapping: str = self.convert_month_integer_to_string_mapping(item)
-            else:
-                string_mapping: str = choice([str(item), self.convert_month_integer_to_string_mapping(item)])
-            string_mapping_list.append(string_mapping)
-
-        return self.convert_list_to_delimited_string(string_mapping_list)
-
-    def convert_tuple_range_to_month_string_mapping(
-        self,
-        integer_tuple: Tuple[int, int],
-        delimiter: Literal["-", "/"],
-        is_start_string_mapping: bool = True,
-        is_end_string_mapping: bool = True,
-    ) -> str:
-        if is_start_string_mapping:
-            start: str = self.convert_month_integer_to_string_mapping(integer_tuple[0])
-        else:
-            start: str = str(integer_tuple[0])
-        if is_end_string_mapping:
-            end: str = self.convert_month_integer_to_string_mapping(integer_tuple[1])
-        else:
-            end: str = str(integer_tuple[1])
-
-        return f"{start}{delimiter}{end}"
-
-    def create_random_month(self, list_length: int) -> str:
-        return choice(
-            [
-                self.ALL,
-                self.ANY,
-                str(randint(*Month.month_range)),
-                self.convert_tuple_range_to_increment_or_range_string(
-                    self.create_random_integer_range_as_tuple(Month.month_range), choice(["-", "/"])
-                ),
-                self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*Month.month_range)), "/"),
-                self.convert_list_to_delimited_string(
-                    self.create_list_of_unique_integers_within_a_range(Month.month_range, randint(2, list_length))
-                ),
-                self.convert_list_to_delimited_string(
-                    self.convert_month_integer_list_to_delimited_string_mapping(
-                        self.create_list_of_unique_integers_within_a_range(Month.month_range, randint(2, list_length)),
-                        choice([True, False]),
-                    )
-                ),
-            ]
-        )
+    def create_random_month(self) -> str:
+        return self.select_random_option_from_list(self.random_choices)
 
 
 class DayOfWeek(Base):
-    week_range: List[int] = [0, 6]
+    def __init__(self):
+        super(DayOfWeek, self).__init__()
+        self.week_range: List[int] = [0, 6]
 
-    weeks_alpha_numeric_mapping: Dict[int, str] = {
-        0: "sun",
-        1: "mon",
-        2: "tue",
-        3: "wed",
-        4: "thu",
-        5: "fri",
-        6: "sat",
-    }
+        self.weeks_alpha_numeric_mapping: Dict[int, str] = {
+            0: "sun",
+            1: "mon",
+            2: "tue",
+            3: "wed",
+            4: "thu",
+            5: "fri",
+            6: "sat",
+        }
+        self.random_choices: Union[Callable, str] = [
+            self.ALL,
+            self.ANY,
+            str(randint(self.week_range)),
+            lambda: self.convert_tuple_range_to_increment_or_range_string(
+                self.create_random_integer_range_as_tuple(self.week_range), choice(["-", "/"])
+            ),
+            lambda: self.convert_tuple_range_to_increment_or_range_string((self.ALL, randint(*self.week_range)), "/"),
+            lambda: self.convert_list_to_delimited_string(
+                self.create_list_of_unique_integers_within_a_range(self.week_range, randint(2, randint(3, 10)))
+            ),
+            lambda: self.convert_integer_list_to_delimited_string_mapping(
+                self.create_list_of_unique_integers_within_a_range(self.week_range, randint(2, randint(3, 10))),
+                self.weeks_alpha_numeric_mapping,
+                choice([True, False]),
+            ),
+        ]
 
-    def create_nth_last_day_of_week_string(self) -> str:
-        return f"""{randint(*DayOfWeek.week_range)}L"""
-
-    def create_nth_weekday_expression(self):
-        return f"""{randint(*DayOfWeek.week_range)}#{randint(1,5)}"""
-
-    def convert_week_integer_to_string_mapping(self, key: int) -> str:
-        return DayOfWeek.weeks_alpha_numeric_mapping[key]
-
-    def convert_week_integer_list_to_delimited_string_mapping(
-        self,
-        integer_list: List[int],
-        make_result_uniform: bool = True,
-    ) -> str:
-        string_mapping_list: List[str] = []
-
-        for item in integer_list:
-            if make_result_uniform:
-                string_mapping: str = self.convert_week_integer_to_string_mapping(item)
-            else:
-                string_mapping: str = choice([str(item), self.convert_week_integer_to_string_mapping(item)])
-            string_mapping_list.append(string_mapping)
-
-        return self.convert_list_to_delimited_string(string_mapping_list)
-
-    def convert_tuple_range_to_week_string_mapping(
-        self,
-        integer_tuple: Tuple[int, int],
-        delimiter: Literal["-", "/"],
-        is_start_string_mapping: bool = True,
-        is_end_string_mapping: bool = True,
-    ) -> str:
-        if is_start_string_mapping:
-            start: str = self.convert_week_integer_to_string_mapping(integer_tuple[0])
-        else:
-            start: str = str(integer_tuple[0])
-        if is_end_string_mapping:
-            end: str = self.convert_week_integer_to_string_mapping(integer_tuple[1])
-        else:
-            end: str = str(integer_tuple[1])
-
-        return f"{start}{delimiter}{end}"
+    def create_random_day_of_week(self) -> str:
+        return self.select_random_option_from_list(self.random_choices)
